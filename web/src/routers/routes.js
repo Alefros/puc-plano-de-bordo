208e5f825e346542d680fc3c29a7051342d9a366
Alefros <alef.r.oliveira@gmail.com>
2023-08-13 22:19:33 -0300

    Adição de controllers e rotas para crud de trips

--- web/src/routers/routes.js
+++ web/src/routers/routes.js
@@ -4,5 +4,9 @@ const mainController = require('../controllers/mainController');
 
 router.get('/', mainController.getVersion);
 router.get('/version', mainController.getVersion);
+router.get('/api/version', mainController.getVersion);
+
+const tripRouter = require('./tripRoutes');
+router.use('/api/trips', tripRouter);
 
 module.exports = router;
